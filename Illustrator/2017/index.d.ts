2846d828bfa42e146f30cb037929c7bb86d59a7b
lumenn <silmelumenn@gmail.com>
2019-11-28 10:36:01 +0100

    Fixed #7

--- Illustrator/2017/index.d.ts
+++ Illustrator/2017/index.d.ts
@@ -9787,7 +9787,7 @@ declare class PageItem {
 	/**
 	 * The value of the Adobe URL tag assigned to this artwork item.
 	 */
-	URL: string;
+	uRL: string;
 
 	/**
 	 * The absolute drawing order of the layer.
